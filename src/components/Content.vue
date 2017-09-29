c40e135b5c93068bc8420ec0d50a1d29abcf46e4
trc-forever <921255465@qq.com>
2017-09-29 17:52:57 +0800

    feat: logout

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Hello App!</h1>
+    <button type="button" @click="logout">Logout</button>
     <p>
       <router-link v-permit="[$router,'/bar']" to="/bar">Go to Bar</router-link>
       <router-link v-permit="[$router,'/foo']" to="/foo">Go to Foo</router-link>
@@ -8,4 +9,14 @@
     </p>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    logout() {
+      sessionStorage.removeItem('token');
+      location.reload();
+    }
+  }
+}
+</script>
 
